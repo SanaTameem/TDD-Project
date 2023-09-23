625ca53dea0ed7a29920f0218dd5311f014e5f7d
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-23 15:32:01 +0430

    Add tests fot factorial methods

---
+++ specs/solver_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../solver'
+
+describe Solver do
+    context "Tests for the factorial method" do
+        it "should raise error if the input number is less than 0" do
+            expect(Solver.new.factorial(-3)).to eq "Please write a number greater than or equal to 0"
+        end
+
+        it "should return 1 if the input number 1" do
+            expect(Solver.new.factorial(1)).to eq 1
+        end
+
+        it "should return 1 if the input number is 0" do
+            expect(Solver.new.factorial(0)).to eq 1
+        end
+
+        it "should return factorial of the input number" do
+            expect(Solver.new.factorial(4)).to eq 24
+        end
+    end
+end
